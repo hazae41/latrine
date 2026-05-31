01ded3a92d8b413f8c6bcde8dcd6f74030f3db9d
hazae41 <4405263+hazae41@users.noreply.github.com>
2026-05-31 23:27:18 +0700

    r

--- run/mod.ts
+++ run/mod.ts
@@ -1,18 +1,12 @@
 // deno-lint-ignore-file no-process-global no-explicit-any
 
-import { base58 } from "@/libs/base58/mod.ts";
 import { IrnClient } from "@/mods/mod.ts";
 import { WcChannel } from "@/mods/wc/channel/mod.ts";
 import { WcInvalidMethodError, WcUserRejectedError } from "@/mods/wc/errors/mod.ts";
 import { WalletConnect, WcPairingParams, WcSessionRequestParams } from "@/mods/wc/mod.ts";
 import { WcPairing } from "@/mods/wc/pairing/mod.ts";
 import { WcSession, WcSessionEventParams, WcSessionProposeParams, WcSessionProposeResult, WcSessionSettleParams } from "@/mods/wc/session/mod.ts";
-import { chaCha20Poly1305 } from "@hazae41/chacha20poly1305";
-import { chaCha20Poly1305Wasm } from "@hazae41/chacha20poly1305-wasm";
-
-await chaCha20Poly1305Wasm.load()
-
-chaCha20Poly1305.set(chaCha20Poly1305.fromWasm(chaCha20Poly1305Wasm))
+import { base58 } from "@hazae41/base58";
 
 const metadata = {
   name: "Latrine",
